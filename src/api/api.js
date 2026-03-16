4b72cc0f6ed53a233e92c2223d014a1209b8b5c7
uone0628 <yu.want.coding@gmail.com>
2026-03-16 21:25:01 +0900

    vercel 연결

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+axios.get(`${import.meta.env.VITE_API_URL}/users`);
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
